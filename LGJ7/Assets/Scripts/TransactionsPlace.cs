984d035622555b6d911b30f4e30d70cba281efc0
Zawisz <zawisza_mateusz@wp.pl>
2020-01-31 23:15:01 +0100

    Full cycle of forge

---
+++ LGJ7/Assets/Scripts/TransactionsPlace.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransactionsPlace : InteractableObject
+{
+    public override void Use(ItemSlot playersItemSlot)
+    {
+        if (playersItemSlot.Item && playersItemSlot.Item.GetComponent<Sword>() 
+            && playersItemSlot.Item.GetComponent<Sword>().IsReady)
+        {
+            Destroy(playersItemSlot.Item);
+            playersItemSlot.RemoveItemFromSlot();
+        }
+    }
+}
