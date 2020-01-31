78c7cf8238223fb08c93167cc967aeb9f5b54122
Zawisz <zawisza_mateusz@wp.pl>
2020-01-31 22:57:20 +0100

    Anvil added

--- LGJ7/Assets/Scripts/Items/Sword.cs
+++ LGJ7/Assets/Scripts/Items/Sword.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Sword : Item
 {
+    [SerializeField]
     private bool isHeated = false;
+    [SerializeField]
     private bool isForged = false;
+    [SerializeField]
     private bool isChilled = false;
+    [SerializeField]
     private bool isReady = false;
+    [SerializeField]
+    private int requiredOres = 2;
 
     public bool IsHeated { get => isHeated; }
     public bool IsForged { get => isForged; }
     public bool IsChilled { get => isChilled; }
     public bool IsReady { get => isReady; }
+    public int RequiredOres { get => requiredOres; }
 
     public void Preheat()
     {
@@ -21,11 +28,13 @@ public class Sword : Item
 
     public void Forge()
     {
+        isHeated = false;
         isForged = true;
     }
 
     public void Chill()
     {
+        isForged = false;
         isChilled = true;
 
         isReady = true;
---
+++ LGJ7/Assets/Scripts/OreContainer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OreContainer : InteractableObject
+{
+    [SerializeField]
+    private GameObject ore;
+
+    public override void Use(ItemSlot playersItemSlot)
+    {
+        //if player has ore in hands put it in container
+        if (playersItemSlot.Item && playersItemSlot.Item.GetComponent<Ore>())
+        {
+            Destroy(playersItemSlot.Item);
+            playersItemSlot.RemoveItemFromSlot();
+        }
+        //if player has empty hands give him ore
+        else if (!playersItemSlot.Item)
+        {
+            playersItemSlot.AddItemToSlot(Instantiate(ore, transform.position, Quaternion.identity));
+        }
+    }
+}
