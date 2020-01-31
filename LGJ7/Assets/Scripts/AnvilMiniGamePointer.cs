176e9a0a597b686edef17d9d26fedb2fcd48bbfb
Zawisz <zawisza_mateusz@wp.pl>
2020-02-01 00:23:28 +0100

    Anvil minigame implementation

---
+++ LGJ7/Assets/Scripts/AnvilMiniGamePointer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnvilMiniGamePointer : MonoBehaviour
+{
+    public bool isInCorrectArea = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "AnvilMiniGameCorrectArea")
+            isInCorrectArea = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "AnvilMiniGameCorrectArea")
+            isInCorrectArea = false;
+    }
+}
