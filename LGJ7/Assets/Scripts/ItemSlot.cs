d50abf2b37b646f8ef6efbd7792cadeef3f9fe04
Zawisz <zawisza_mateusz@wp.pl>
2020-01-31 20:43:30 +0100

    Push

---
+++ LGJ7/Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSlot : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject slot = null;
+
+    public GameObject Slot { get => slot; set => slot = value; }
+
+    public void AddItemToSlot(GameObject item)
+    {
+        if (!slot)
+            slot = item;
+        else
+            Debug.Log("Player's slot is not empty!");
+    }
+
+    public void RemoveItemFromSlot()
+    {
+        slot = null;
+        Debug.Log("Player's slot is empty. Item has been removed!");
+    }
+}
---
+++ LGJ7/Assets/Scripts/ItemType.cs
@@ -0,0 +1,7 @@
+﻿public enum ITEMTYPE
+{
+    Sword,
+    Armor,
+    Shield
+}
+
