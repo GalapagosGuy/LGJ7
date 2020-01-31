042b9df9afd88d968f0937b4fb775d893c43d0ca
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2020-01-31 22:14:09 +0100

    Furnace wip

--- LGJ7/Assets/Scripts/Furnace.cs
+++ LGJ7/Assets/Scripts/Furnace.cs
@@ -7,12 +7,29 @@ public class Furnace : InteractableObject
 
     public override void Use(ItemSlot playersItemSlot)
     {
-        throw new System.NotImplementedException();
+        if (playersItemSlot.Item)
+        {
+            itemSlot.AddItemToSlot(playersItemSlot.Item);
+
+            playersItemSlot.RemoveItemFromSlot();
+
+            startHeating();
+        }
+        else if (!playersItemSlot.Item && itemSlot.Item.GetComponent<Sword>().IsHeated)
+        {
+            playersItemSlot.AddItemToSlot(itemSlot.Item);
+
+            itemSlot.RemoveItemFromSlot();
+        }
+
     }
     void Update()
     {
         
     }
+    private void startHeating()
+    {
 
+    }
     
 }
