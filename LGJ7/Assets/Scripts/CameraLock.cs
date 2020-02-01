d1cd5e76def588129d3955e131bcc844f438ed94
Zawisz <zawisza_mateusz@wp.pl>
2020-02-01 02:14:07 +0100

    Goblins, again...

---
+++ LGJ7/Assets/Scripts/CameraLock.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLock : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    private float distance = 10;
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, this.transform.position.y, target.position.z - distance);
+    }
+}
