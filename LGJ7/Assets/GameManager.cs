e3c7360d267de5dcaac15d60cec0843e30b03d32
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2020-02-01 06:04:03 +0100

    Scene and points

---
+++ LGJ7/Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text geldText;
+    private int geld = 0;
+
+    public int Geld { get => geld; set => geld = value; }
+
+    public void AddGeld(int geldAdded)
+    {
+        geld += geldAdded;
+    }
+    void Update()
+    {
+        geldText.text = "Geld: " + geld;
+    }
+}
--- LGJ7/Assets/Scripts/TransactionsPlace.cs
+++ LGJ7/Assets/Scripts/TransactionsPlace.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class TransactionsPlace : InteractableObject
 {
+    [SerializeField]
+    private GameManager gameManager;
     public override void Use(ItemSlot playersItemSlot)
     {
         if (playersItemSlot.Item && playersItemSlot.Item.GetComponent<Sword>() 
             && playersItemSlot.Item.GetComponent<Sword>().IsReady)
         {
+            gameManager.AddGeld(100);
             Destroy(playersItemSlot.Item);
             playersItemSlot.RemoveItemFromSlot();
         }
