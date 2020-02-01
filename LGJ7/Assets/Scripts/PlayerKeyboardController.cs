aebbdd2cc0f7fd92373dd16fa562eaeffe428f64
B.Krukowski <38346579+OrangeInside@users.noreply.github.com>
2020-02-01 02:51:40 +0100

    Saved scene + working controller

--- LGJ7/Assets/Scripts/PlayerKeyboardController.cs
+++ LGJ7/Assets/Scripts/PlayerKeyboardController.cs
@@ -13,7 +13,6 @@ public class PlayerKeyboardController : MonoBehaviour
     {
         if(Input.GetButtonDown(playerAction))
         {
-            
             playerController.ActivateInteractableObject();
         }
       
